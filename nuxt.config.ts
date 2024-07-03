6292b91c5fbf2757aaf0eb6dfd408e94520ff7d2
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-07-04 00:15:21 +0300

    fix bootstrap js issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['bootstrap/dist/css/bootstrap.min.css'],
+  app: {
+    head: {
+      title: 'My Awesome App',
+      meta: [{ charset: 'utf-8' }],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js',
+          type: 'text/javascript',
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   ssr: true,
 });
