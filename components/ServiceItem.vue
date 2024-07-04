bd36ab0c57af6bf5d8db62ea25b99fb310021927
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-07-04 09:34:52 +0300

    create a separate serviceItem component

---
+++ components/ServiceItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-lg-3 col-sm-6 mb-30 mb-lg-0">
+    <div class="service-card style-3">
+      <div class="icon">
+        <img :src="service?.icon" alt="" />
+      </div>
+      <div class="info">
+        <h5 class="title">{{ service?.title }}</h5>
+        <div class="text">
+          {{ service?.details }}
+        </div>
+        <NuxtLink to="/page-portfolio-5"
+          ><span>see projects</span> <i class="bi bi-chevron-right ms-1"></i
+        ></NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  service: Object,
+});
+</script>
--- components/Services.vue
+++ components/Services.vue
@@ -8,27 +8,11 @@
         ></NuxtLink>
       </div>
       <div class="row">
-        <div
-          class="col-lg-3 col-sm-6 mb-30 mb-lg-0"
+        <ServiceItem
           v-for="(service, index) in services"
           :key="index"
-        >
-          <div class="service-card style-3">
-            <div class="icon">
-              <img :src="service.icon" alt="" />
-            </div>
-            <div class="info">
-              <h5 class="title">{{ service.title }}</h5>
-              <div class="text">
-                {{ service.details }}
-              </div>
-              <NuxtLink to="/page-portfolio-5"
-                ><span>see projects</span>
-                <i class="bi bi-chevron-right ms-1"></i
-              ></NuxtLink>
-            </div>
-          </div>
-        </div>
+          :service="service"
+        />
       </div>
     </div>
   </section>
