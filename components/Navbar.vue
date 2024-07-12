cc482df79104b32505d6c24bf3f2e3790aa516b6
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-07-12 21:52:43 +0300

    add rounded menu items in the navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,136 +1,10 @@
 <template>
-  <!-- <nav
-    class="py-5 flex flex-wrap justify-between align-center grow-1 absolute w-full"
-    ref="navbarRef"
-  >
-    <div class="container">
-      <a class="d-flex align-items-center" href="#">
-        <img src="/meftihe-logo-white.svg" alt="Meftihe logo" class="w-28" />
-        <div class="ms-2 lh-1">
-          <h3 class="text-white lh-1">Meftihe</h3>
-          <span class="text-white">IT Solutions</span>
-        </div>
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav m-auto mb-2 mb-lg-0 text-uppercase">
-          <li
-            class="nav-item dropdown"
-            @mousemove="handleMouseMove"
-            @mouseleave="handleMouseLeave"
-          >
-            <a
-              class="nav-link active dropdown-toggle"
-              href="#"
-              id="navbarDropdown1"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Homes
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
-              <li>
-                <NuxtLink to="#" class="dropdown-item"> Data Analysis</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#" class="dropdown-item">App Landing</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#" class="dropdown-item"
-                  >Software Company</NuxtLink
-                >
-              </li>
-            </ul>
-          </li>
-          <li
-            class="nav-item dropdown"
-            @mousemove="handleMouseMove"
-            @mouseleave="handleMouseLeave"
-          >
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown2"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              pages
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
-              <li>
-                <NuxtLink to="#" class="dropdown-item">about</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#" class="dropdown-item">product</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#" class="dropdown-item">services</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#" class="dropdown-item">shop</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="#" class="dropdown-item">single project</NuxtLink>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <NuxtLink to="#" class="nav-link"> portfolio </NuxtLink>
-          </li>
-          <li class="nav-item">
-            <NuxtLink to="#" class="nav-link"> blog </NuxtLink>
-          </li>
-          <li class="nav-item">
-            <NuxtLink to="#" class="nav-link"> contact </NuxtLink>
-          </li>
-        </ul>
-        <div class="nav-side">
-          <div class="d-inline-flex align-items-center ps-4">
-            <a href="#" class="search-icon me-4">
-              <Icon name="bi:search" class="text-white"></Icon>
-            </a>
-            <div
-              class="dropdown"
-              @mousemove="handleMouseMove"
-              @mouseleave="handleMouseLeave"
-            >
-              <button
-                class="d-inline-flex dropdown-toggle border-0 p-0 bg-transparent img-cover text-white"
-                type="button"
-                id="dropdownMenuButton1"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <Icon name="ion:language" size="20"></Icon>
-              </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><a class="dropdown-item" href="#">English</a></li>
-                <li><a class="dropdown-item" href="#">አማርኛ</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav> -->
   <div
-    class="navbar top-0 w-full z-10 bg-transparent absolute z-1000 text-white md:px-28"
+    class="navbar top-0 w-full z-10 bg-transparent absolute z-1000 text-white md:px-28 md:py-10"
   >
     <div class="navbar-start">
       <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost md:hidden">
+        <div tabindex="0" role="button" class="btn-ghost inline-flex lg:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-5 w-5"
@@ -148,7 +22,7 @@
         </div>
         <ul
           tabindex="0"
-          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          class="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow"
         >
           <li><a>Item 1</a></li>
           <li>
@@ -161,25 +35,40 @@
           <li><a>Item 3</a></li>
         </ul>
       </div>
-      <a class="btn btn-ghost text-xl">daisyUI</a>
+
+      <a class="flex items-center" href="#">
+        <img
+          src="/meftihe-logo-white.svg"
+          alt="Meftihe logo"
+          class="w-12 md:w-28"
+        />
+        <div
+          class="ml-1 space-y-0 flex md:flex-col items-center md:items-baseline gap-2 lg:gap-0"
+        >
+          <h3 class="text-white lg:text-xl leading-none">Meftihe</h3>
+          <span class="text-white lg:text-sm leading-none">IT Solutions</span>
+        </div>
+      </a>
     </div>
     <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
+      <ul class="menu menu-horizontal px-1 [&_li>*]:rounded-full">
+        <li class="hover:bg-base-200 rounded-full">
+          <a>Item 1</a>
+        </li>
         <li>
           <details>
-            <summary>Parent</summary>
+            <summary class="rounded-full hover:bg-base-200">Parent</summary>
             <ul class="p-2">
               <li><a>Submenu 1</a></li>
               <li><a>Submenu 2</a></li>
             </ul>
           </details>
         </li>
-        <li><a>Item 3</a></li>
+        <li class="hover:bg-base-200 rounded-full"><a>Item 3</a></li>
       </ul>
     </div>
     <div class="navbar-end">
-      <a class="btn">Button</a>
+      <a class="btn btn-sm md:btn-md">Button</a>
     </div>
   </div>
 </template>
