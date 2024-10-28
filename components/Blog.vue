6c922eb90e6e50e9f3c12249248b8f02da62e327
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-10-28 18:05:54 +0300

    move clients to its own component

--- components/Blog.vue
+++ components/Blog.vue
@@ -122,23 +122,7 @@
           </NuxtLink>
         </div>
       </div>
-      <div class="mt-24">
-        <div class="flex flex-wrap items-center">
-          <div
-            class="basis-1/2 lg:basis-2/12"
-            v-for="(client, index) in data.clients"
-            :key="index"
-          >
-            <a href="#" class="block my-5">
-              <img
-                :src="client"
-                class="opacity-60 grayscale hover:grayscale-0"
-                alt=""
-              />
-            </a>
-          </div>
-        </div>
-      </div>
+      <Clients />
     </div>
     <img
       src="/blog/v_lines.png"
---
+++ components/Clients.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-24">
+    <div class="flex flex-wrap items-center">
+      <div
+        class="basis-1/2 lg:basis-2/12"
+        v-for="(client, index) in data.clients"
+        :key="index"
+      >
+        <a href="#" class="block my-5">
+          <img
+            :src="client"
+            class="opacity-60 grayscale hover:grayscale-0"
+            alt=""
+          />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import data from "../data/blog.json";
+</script>
